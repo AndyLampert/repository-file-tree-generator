4fcdbfa68177500a24ab6b699edf7d2c03f21eb0
AndyLampert <lamperta@gmail.com>
2014-05-26 11:21:00 -0600

    setting up repo

---
+++ app.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var app = express();
+app.set('view engine', 'jade');
+app.set('views', __dirname + '/views');
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser());
+
+app.get('/', function(req, res) {
+	res.render('index');
+});
+
+var server = app.listen(4474, function() {
+	console.log('Express server listening on port ' + server.address().port);
+});
